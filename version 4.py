50bc5adbe80d394cb00d343917ffdb8bbe24dcad
fatoumata-fofana <ftimtim17@gmail.com>
2025-12-15 11:48:22 -0500

    "Version 4: Combined all functions to play a single round".

---
+++ version 4.py
@@ -0,0 +1,25 @@
+import random
+
+def get_user_choice():
+  return input("Choose rock, paper, or scissors:")
+
+
+def get_computer_choice():
+  choices = ["rock", "paper", "scissors"]
+  return random.choice(choices)
+  
+def determine_winner(user_choice, computer_choice):
+  if user_choice == computer_choice:
+    return "it's a tie!"
+  elif user_choice == "rock" and computer_choice == "scissors":
+    return "you win!"
+  elif user_choice == "paper" and computer_choice == "rock":
+    return "you win!"
+  elif user_choice == "scissors"  and computer_choice =="paper":
+    return "you win!"
+  else:
+    return "you lose!"
+
+user_choice = get_user_choice()
+cpu_choice = get_computer_choice()
+print(determine_winner(user_choice, cpu_choice))
